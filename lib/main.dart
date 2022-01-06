43c0e3ee089c5d0abbd5db90cf4b2680c96e6a3a
Lucasmercado101 <lucasmercado101@gmail.com>
2022-01-06 16:59:23 -0300

    adding logic

--- lib/main.dart
+++ lib/main.dart
@@ -37,38 +37,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: BookCard(
+          title: 'The Psychology of Money', authors: const ['Morgan Housel']),
+    );
+  }
+}
+
+class BookCard extends StatelessWidget {
+  String title;
+  List<String> authors;
+
+  BookCard({Key? key, required this.title, required this.authors})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+              "https://covers.openlibrary.org/b/isbn/9780857197689-L.jpg"),
+          Text(title),
+          Text(authors.length > 1 ? authors.join(', ') : authors[0]),
+        ],
       ),
     );
   }
