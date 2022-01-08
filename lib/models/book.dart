a1f9134eb5e5bf35b28c58755aebdece308975bd
Lucasmercado101 <lucasmercado101@gmail.com>
2022-01-08 18:11:17 -0300

    added book model

---
+++ lib/models/book.dart
@@ -0,0 +1,12 @@
+class Book {
+  final String title;
+  final List<String> authors;
+  final int pages;
+  final String imageUrl;
+
+  const Book(
+      {required this.title,
+      required this.authors,
+      required this.pages,
+      required this.imageUrl});
+}
