fd63bd4af3388c737648e40d415d506b8580643e
Lucasmercado101 <lucasmercado101@gmail.com>
2022-01-10 15:12:56 -0300

    WIP

--- lib/pages/new_book.dart
+++ lib/pages/new_book.dart
@@ -8,14 +8,44 @@ class NewBookPage extends StatefulWidget {
 }
 
 class _NewBookState extends State<NewBookPage> {
+  String? _title;
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("New Book"),
       ),
-      body: Center(
-        child: Text("New Book"),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Title",
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _title = value;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // TODO: Add new book and save
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
       ),
     );
   }
